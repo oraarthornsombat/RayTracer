4cf592966286427d5b4019e5754c30562bf7de6b
oraarthornsombat <oratanya@hotmail.com>
2016-03-18 17:44:38 +0000

    Finished raySphere intersects method

--- Assignment2/Ray/raySphere.todo.cpp
+++ Assignment2/Ray/raySphere.todo.cpp
@@ -7,55 +7,61 @@
 //  Ray-tracing stuff //
 ////////////////////////
 double RaySphere::intersect(Ray3D ray,RayIntersectionInfo& iInfo,double mx){
-	//return -1;
-		bool mxBound = false;
-	if (mx > 0) mxBound = true;
-
-	//printf("Running Sphere Intersect\n");
-
+	//If the ray hits the shape and either mx is less than zero or the intersection occurs
+	//at a distance less than t along the ray the intersection information in iInfo is set 
+	//and a value greater than 0 is returned. Otherwise, -1 is returned. 
+	
+	bool intersects = false;
+ 	//|P - O|^2 - r^2 = 0 
 	double rsquare = pow(radius,2);
 
+	//L = O - P0
 	Point3D l = center - ray.position;
+	
+	//tca = L • V
 	double tca = l.dot(ray.direction);
+	
+	//d^2 = L • L - tca^2
 	double dsquare = l.dot(l) - (pow(tca,2));
 
+	//Instersection with sphere? No.
+	if (dsquare > rsquare){
+		intersects=false;
+		return -1;
+	} else {
+		//Else, intersects
+		intersects=true;
+		//thc = sqrt(r^2 - d^2)
+		double thc = sqrt(rsquare - dsquare);
+		double t1 = tca - thc;
+		double t2 = tca + thc;
 
-	//determines if there is an intersection with the sphere first
-	if (dsquare > rsquare) return -1;
-
-
-	//We know it intersects, now we find the values to return
-	double thc = sqrt(rsquare - dsquare);
-	double t1 = tca - thc;
-	double t2 = tca + thc;
-
-	double t = -1;
-	if (t1 > t2)
-	{
-		t = t2;
-	}
-	else
-	{
-		t = t1;
-	}
-	Point3D p = ray.position + ray.direction*t;
-	double mag = sqrt(
-		pow(p.p[0] - center.p[0],2) + 
-		pow(p.p[1] - center.p[1],2) + 
-		pow(p.p[2] - center.p[2],2));
-
-	Point3D normal = (p - center) / mag;
-	/*printf("mag = %f\n", mag);
-	printf("p(%f, %f, %f)\n", p.p[0], p.p[1], p.p[2]);
-	printf("center(%f, %f, %f)\n", center.p[0], center.p[1], center.p[2]);
-	printf("normal(%f, %f, %f)\n", normal.p[0], normal.p[1], normal.p[2]);*/
-
+		double t = -1;
+		//return first intersection point
+		if (t1 > t2)
+		{
+			t = t2;
+		}
+		else
+		{
+			t = t1;
+		}
 	
-	iInfo.iCoordinate = p;
-	iInfo.normal = normal;
-	iInfo.material = material;
+	//Ray: P = P0 + tV
+	Point3D p = ray.position + ray.direction*t;
+	//||P-O||
+	double magnitude = sqrt(pow(p.p[0] - center.p[0],2) + pow(p.p[1] - center.p[1],2) + pow(p.p[2] - center.p[2],2));
 
+	//N = (P - O) / ||P - O||
+	Point3D normal = (p - center) / magnitude;
+	
+	if ((intersects && mx<0)){
+		iInfo.iCoordinate = p;
+		iInfo.normal = normal;
+	}
+	
 	return t;
+	}
 }
 BoundingBox3D RaySphere::setBoundingBox(void){
 	return bBox;
